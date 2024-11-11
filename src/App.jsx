94960592522ee111a1ad89b617bf5d8a2a7da36d
Aaron Hirschhorn <a.l.hirschhorn@wustl.edu>
2024-11-11 17:03:07 -0600

    setValue X on click

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { useState } from 'react';
 
 function Square() {
 
-  const [value, setVallue] = useState(null);
+  const [value, setValue] = useState(null);
 
   function handleClick() {
-    console.log("clicked!");
+    setValue('X');
   }
 
   return (
